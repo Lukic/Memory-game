6a1b957a9571e35eabe44a162459016455dccc92
Srdjan Lukic <srdjan@lukic.nu>
2021-08-26 11:56:16 +0200

    Clean up

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
-// Billeder til opgaven
+/*
+  For at aktivere et kort, tilføj en css class med navnet active
+  eks. object.classList.add("active"); . men husk også at fjerne class hvis 
+  kortet ikke matcher. .classList.remove("active");
+*/
+
+// Step 1. Tilføj click event på alle kort holder elemente <figure>.
+// Step 2. Tilføj check om 2 billeder som er aktive matcher.
+
+// Ekstra opgaver.
+// 1. Indbyg en score som give + point ved korret match, og - point ved forkert.
+// 2. Indbyg en reset knap så spillet kan genstrate.
+// 3. Udskriv billeder i tilfældig rækkefølge.
+/*
+eks ved at bruge en array:
 const memoryPictureUrls = [
   "https://picsum.photos/seed/memory_1/300/300",
   "https://picsum.photos/seed/memory_2/300/300",
@@ -13,19 +27,5 @@ const memoryPictureUrls = [
   "https://picsum.photos/seed/memory_5/300/300",
   "https://picsum.photos/seed/memory_6/300/300",
 ];
-
-/*
-  For at aktivere et kort, tilføj en css class med navnet active
-  eks. object.classList.add("active"); . men husk også at fjerne class hvis 
-  kortet ikke matcher. .classList.remove("active");
 */
-
-// Step 1. Byg en funktion som tilføjer billeder fra array i HTML img tag.
-// Step 2. Tilføj click event på alle kort holder elemente <figure>.
-// Step 3. Tilføj check om 2 billeder som er aktive matcher.
-
-// Ekstra opgaver.
-// 1. Indbyg en score som give + point ved korret match, og - point ved forkert.
-// 2. Indbyg en reset knap så spillet kan genstrate.
-// 3. Udskriv billeder i tilfældig rækkefølge.
 // 4. Når spillet er forbi, brug confetti.js til at vise confetti på skærmen. Mere info her : https://github.com/abelmoricz/abelmoricz.github.io/tree/9eac02160de7bb57170441a441db96b36e8341d8/confetti.js-master
